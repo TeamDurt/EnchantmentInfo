358198fb271f91402880224da7a47fad921ac23a
Nikonell <rozhkovn08@gmail.com>
2024-07-08 01:06:03 +0700

    initial commit

---
+++ common/src/main/java/com/teamdurt/enchantmentinfo/Constants.java
@@ -0,0 +1,11 @@
+package com.teamdurt.enchantmentinfo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "enchantmentinfo";
+	public static final String MOD_NAME = "Enchantment Info";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
