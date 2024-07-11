94b1a1821247cfc957d88fa103991b7baf29500f
mirik <miriklevshenkov@gmail.com>
2024-07-11 23:48:47 +0200

    added line tooltips

---
+++ common/src/main/java/com/teamdurt/enchantmentinfo/frontend/tooltip/line/ColoredLineTooltip.java
@@ -0,0 +1,36 @@
+package com.teamdurt.enchantmentinfo.frontend.tooltip.line;
+
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
+import org.jetbrains.annotations.NotNull;
+
+public class ColoredLineTooltip implements ClientTooltipComponent {
+    int color;
+    int height;
+    int width = 2;
+
+    public ColoredLineTooltip(int color, int height) {
+        this.color = color;
+        this.height = height;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public int getWidth(@NotNull Font font) {
+        return width;
+    }
+
+    @Override
+    public void renderImage(@NotNull Font font, int x, int y, @NotNull GuiGraphics guiGraphics) {
+        guiGraphics.fill(x, y, x + getWidth(font), y + getHeight(), getColor());
+    }
+}
---
+++ common/src/main/java/com/teamdurt/enchantmentinfo/frontend/tooltip/line/RedLineTooltip.java
@@ -0,0 +1,9 @@
+package com.teamdurt.enchantmentinfo.frontend.tooltip.line;
+
+public class RedLineTooltip extends ColoredLineTooltip {
+    public static final int RED_COLOR = -1240534; //#ffed122a
+
+    public RedLineTooltip(int height) {
+        super(RED_COLOR, height);
+    }
+}
