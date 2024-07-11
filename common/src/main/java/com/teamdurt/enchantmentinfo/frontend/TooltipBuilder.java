5a3db4bb964fdf69b13b748ca0fde5d939c8bb61
mirik <miriklevshenkov@gmail.com>
2024-07-11 23:51:07 +0200

    added incompatible enchantments to TooltipBuilder

--- common/src/main/java/com/teamdurt/enchantmentinfo/frontend/TooltipBuilder.java
+++ common/src/main/java/com/teamdurt/enchantmentinfo/frontend/TooltipBuilder.java
@@ -1,13 +1,20 @@
 package com.teamdurt.enchantmentinfo.frontend;
 
 import com.mojang.datafixers.util.Pair;
+import com.teamdurt.enchantmentinfo.category.ModEnchantmentCategory;
 import com.teamdurt.enchantmentinfo.enchantment_data.EnchantmentDataManager;
+import com.teamdurt.enchantmentinfo.frontend.tooltip.line.GreenLineTooltip;
+import com.teamdurt.enchantmentinfo.frontend.tooltip.ParentTooltip;
+import com.teamdurt.enchantmentinfo.frontend.tooltip.line.RedLineTooltip;
+import com.teamdurt.enchantmentinfo.frontend.tooltip.TexturedTooltip;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
@@ -51,7 +58,45 @@ public class TooltipBuilder {
     }
 
     private static void addCustomComponents(List<Component> components, ListTag enchantmentTags) {
+        List<ClientTooltipComponent> tooltips = Lists.newArrayList();
 
+        List<Pair<Enchantment, Integer>> enchantments = getEnchantmentsFromTag(enchantmentTags);
+
+        for (Pair<Enchantment, Integer> enchantmentWithLevel : enchantments) {
+            //parent tooltip
+            ParentTooltip mainParent = new ParentTooltip();
+
+            //enchantment name
+            mainParent.addChild(getEnchantmentName(enchantmentWithLevel));
+            //incompatible enchantments
+            mainParent.addChild(parseIncompatibleEnchantments(enchantmentWithLevel));
+
+            //adding the whole thing to tooltips
+            tooltips.add(mainParent);
+        }
+
+        components.addAll(FakeComponent.tooltipsToComponents(tooltips));
+    }
+
+    private static ClientTooltipComponent parseIncompatibleEnchantments(Pair<Enchantment, Integer> enchantmentWithLevel) {
+        List<Enchantment> incompatibleEnchantments = enchantmentDataManager.getIncompatibleEnchantments(enchantmentWithLevel.getFirst());
+
+        if (incompatibleEnchantments.isEmpty()) return null;
+
+        ParentTooltip parent = new ParentTooltip(ParentTooltip.Orientation.HORIZONTAL, 2);
+
+        ParentTooltip incompatiblesList = new ParentTooltip();
+        for (Enchantment enchantment : incompatibleEnchantments) {
+            incompatiblesList.addChild(
+                    getEnchantmentName(enchantment)
+            );
+        }
+
+        RedLineTooltip redLine = new RedLineTooltip(incompatiblesList.getHeight() - 3);
+        parent.addChild(redLine);
+        parent.addChild(incompatiblesList);
+
+        return parent;
     }
 
     private static void addShiftMessage(List<Component> components, boolean shouldHold) {
