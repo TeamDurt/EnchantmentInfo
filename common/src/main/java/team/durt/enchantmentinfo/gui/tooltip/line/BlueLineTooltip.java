3677e3f5512fccf549d2b1824c1dc616a8b63b89
mirik <miriklevshenkov@gmail.com>
2024-07-18 12:28:27 +0200

    added BlueLineTooltip for future grouping

---
+++ common/src/main/java/team/durt/enchantmentinfo/gui/tooltip/line/BlueLineTooltip.java
@@ -0,0 +1,9 @@
+package team.durt.enchantmentinfo.gui.tooltip.line;
+
+public class BlueLineTooltip extends ColoredLineTooltip {
+    public static final int BLUE_COLOR = -13273872; //#ff3574f0
+
+    public BlueLineTooltip(int height) {
+        super(BLUE_COLOR, height);
+    }
+}
